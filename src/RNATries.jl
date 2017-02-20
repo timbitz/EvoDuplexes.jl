446049cb331c67e5d006ddb6adce0a84dfc130c9
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2017-02-20 12:23:08 -0800

    rename

---
+++ src/RNATries.jl
@@ -0,0 +1,16 @@
+#__precompile__()
+
+module RNATries
+
+using Bio.Seq
+using Bio.Intervals
+
+importall Bio.Intervals
+
+include("pairs.jl")
+include("energy.jl")
+include("duplex.jl")
+include("intervals.jl")
+include("trie.jl")
+
+end
